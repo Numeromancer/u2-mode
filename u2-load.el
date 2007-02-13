d7ca7f2a58ed4b02543af84e0d24b8199690ed32
numeromancer <numeromancer>
2007-02-13 19:11:37 +0000

    I wrapped the `load's in a let which ensures that the files are loaded from the current directory.

--- u2-load.el
+++ u2-load.el
@@ -1,7 +1,8 @@
 ;; -*- Emacs-lisp -*-
 
-(load "u2-cache.el")
-(load "unibasic.el")
-(load "unidata.el")
-(load "u2-compile.el")
-(load "u2-dired.el")
+(let ((load-file (cons "." load-file)))
+  (load "u2-cache.el")
+  (load "unibasic.el")
+  (load "unidata.el")
+  (load "u2-compile.el")
+  (load "u2-dired.el"))
