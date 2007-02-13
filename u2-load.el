c9fe5a0ba8ad4fdfe7db404eeb38204a3a648cc7
numeromancer <numeromancer>
2007-02-13 19:19:55 +0000

    The last needed ``./'' before each file name.  This one really works. Really.

--- u2-load.el
+++ u2-load.el
@@ -1,8 +1,8 @@
 ;; -*- Emacs-lisp -*-
 
 (let ((load-path (cons "." load-path)))
-  (load "u2-cache.el")
-  (load "unibasic.el")
-  (load "unidata.el")
-  (load "u2-compile.el")
-  (load "u2-dired.el"))
+  (load "./u2-cache.el")
+  (load "./unibasic.el")
+  (load "./unidata.el")
+  (load "./u2-compile.el")
+  (load "./u2-dired.el"))
